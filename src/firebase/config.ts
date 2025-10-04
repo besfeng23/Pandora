96aa33b7910a9e419f1996fe0119e781f6e015c8
lawa elem <unclejose0@gmail.com>
2025-10-04 22:40:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6440176509-5e155",
+  "appId": "1:1024674802573:web:ec8facbebdd73a836a6bc4",
+  "apiKey": "AIzaSyDlzYS0MZI6gDvz5xpvdfUImAOXabmPTfs",
+  "authDomain": "studio-6440176509-5e155.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1024674802573"
+};

88c151582ee06762d132b99944c92a76ff9f73c7
lawa elem <unclejose0@gmail.com>
2025-10-04 15:06:11 +0000

    Try fixing this error: `Runtime Error: Error: Input is not defined. Erro

--- src/app/connections/page.tsx
+++ src/app/connections/page.tsx
@@ -23,6 +23,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
   Sheet,

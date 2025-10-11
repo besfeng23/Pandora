6fad9f17e1d926992a19ff16af45503cb5af2b7c
lawa elem <unclejose0@gmail.com>
2025-10-11 07:34:48 +0000

    Remove this (_for element <SettingsHeader>_)

--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -1,9 +1,7 @@
 
-
 "use client";
 
 import { SettingsNav } from "@/components/settings/settings-nav";
-import { SettingsHeader } from "@/components/settings/settings-header";
 
 export default function SettingsLayout({
   children,
@@ -12,7 +10,6 @@ export default function SettingsLayout({
 }) {
   return (
     <div className="space-y-6">
-      <SettingsHeader />
       <div className="grid grid-cols-1 md:grid-cols-12 gap-6">
         <div className="col-span-12 md:col-span-3 lg:col-span-2">
           <SettingsNav />

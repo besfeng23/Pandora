bac18410edc80b95a2ef5b6cd4fa97dda72902ec
besfeng23 <136943241+besfeng23@users.noreply.github.com>
2025-12-23 10:28:27 +0800

    Create Firebase configuration file with environment variables

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,11 @@
+'use client';
+
 export const firebaseConfig = {
-  "projectId": "studio-6440176509-5e155",
-  "appId": "1:1024674802573:web:ec8facbebdd73a836a6bc4",
-  "apiKey": "AIzaSyDlzYS0MZI6gDvz5xpvdfUImAOXabmPTfs",
-  "authDomain": "studio-6440176509-5e155.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1024674802573"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || '',
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || '',
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || '',
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '',
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || '',
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
